ebeb32793b3fde6c5e03604ddd47bd5ac2babaae
Oyudo Gerald <gerald.oyudo@gmail.com>
2018-06-15 18:21:40 +0100

    first commit

---
+++ src/main/java/com/gerald/hangout/assistant/hangoutassistant/HangoutAssistantApplication.java
@@ -0,0 +1,12 @@
+package com.gerald.hangout.assistant.hangoutassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangoutAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HangoutAssistantApplication.class, args);
+	}
+}
---
+++ src/test/java/com/gerald/hangout/assistant/hangoutassistant/HangoutAssistantApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gerald.hangout.assistant.hangoutassistant;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HangoutAssistantApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
